08c49af3c3056041008393c4e9625dca7243de53
angelDi0 <angel.diosdado1239@gmail.com>
2025-05-22 23:34:27 -0600

    Implementacion de la conexion a la base de datos

--- index.js
+++ index.js
@@ -1,29 +1,32 @@
-function validateForm(event) {
-    event.preventDefault(); 
-    const usuario = document.getElementById('usuario').value.trim();
-    const contrasenia = document.getElementById('contrasenia').value.trim();
-    const errorMessage = document.getElementById('errorMessage');
-
-    errorMessage.style.display = 'none';
-    errorMessage.textContent = '';
-
-    if (!usuario || !/^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(usuario)) {
-        errorMessage.textContent = 'Por favor, ingresa un correo electrónico válido.';
-        errorMessage.style.display = 'block';
-        return false;
-    }
-
-    if (!contrasenia) {
-        errorMessage.textContent = 'La contraseña no puede estar vacía.';
-        errorMessage.style.display = 'block';
-        return false;
-    }
-
-    document.getElementById('loginForm').submit();
-    return true;
-}
-
 window.onload = function() {
+    const form = document.getElementById('loginForm');
+    form.addEventListener('submit', function(event) {
+        console.log("Enviando datos: ", usuario, contrasenia);
+        
+        event.preventDefault();
+
+        const usuario = document.getElementById('usuario').value.trim();
+        const contrasenia = document.getElementById('contrasenia').value.trim();
+        const errorMessage = document.getElementById('errorMessage');
+
+        errorMessage.style.display = 'none';
+        errorMessage.textContent = '';
+
+        if (!usuario || !/^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(usuario)) {
+            errorMessage.textContent = 'Por favor, ingresa un correo electrónico válido.';
+            errorMessage.style.display = 'block';
+            return;
+        }
+
+        if (!contrasenia) {
+            errorMessage.textContent = 'La contraseña no puede estar vacía.';
+            errorMessage.style.display = 'block';
+            return;
+        }
+
+        form.submit();
+    });
+
     const urlParams = new URLSearchParams(window.location.search);
     const error = urlParams.get('error');
     const errorMessage = document.getElementById('errorMessage');
